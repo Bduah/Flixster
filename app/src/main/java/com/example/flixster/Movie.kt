21280a6288e0444f8c972ca984c9f6e227136ac2
Bduah <bduah@smith.edu>
2022-03-01 16:33:29 -0500

    Adding the second phase of flex part 2.

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,15 +1,23 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.IgnoredOnParcel
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
+
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
     private val backdropPath: String,
-){
+):Parcelable{
+    @IgnoredOnParcel
     val backdropPathUrl = "https://image.tmdb.org/t/p/w780/$backdropPath"
+
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object{
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -18,7 +26,9 @@ data class Movie (
                 val movieJson = movieJsonArray.getJSONObject(i)
                 movies.add(
                     Movie(
+
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview"),
